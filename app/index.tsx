4a178486213bc2695214463a5164b5e04e207d08
ozan-fn <23sa11a032@students.amikompurwokerto.ac.id>
2025-10-29 21:34:41 +0700

    add: nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 justify-center items-center bg-black">
+            <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
